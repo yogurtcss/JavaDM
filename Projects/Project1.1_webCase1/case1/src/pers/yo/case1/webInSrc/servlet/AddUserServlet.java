031335a7633b165bbe0233f534eb0ec3b65845d3
yogurtcss <1806631955@qq.com>
2019-12-25 10:47:07 +0800

    upload

---
+++ Projects/Project1.1_webCase1/case1/src/pers/yo/case1/webInSrc/servlet/AddUserServlet.java
@@ -0,0 +1,19 @@
+package pers.yo.case1.webInSrc.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/addUserServlet") //为 注解的servlet模板 修改的代码，加个斜杠。一般servlet的首字母小写嗷！
+public class AddUserServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doPost(request, response); //为 注解的servlet模板 新增的代码
+    }
+}
