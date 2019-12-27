da12289cd4609d4a218b12acdf45d14cd15c64fc
yogurtcss <1806631955@qq.com>
2019-12-27 16:26:06 +0800

    upload

--- Projects/Project1.1_webCase1/case1/src/pers/yo/case1/service/impl/UserServiceImpl.java
+++ Projects/Project1.1_webCase1/case1/src/pers/yo/case1/service/impl/UserServiceImpl.java
@@ -45,42 +45,57 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public void delSeletedUser(String[] ids) {
-        if( ids!=null && ids.length>0 ){
-            for( String id: ids ){
-                dao.delete( Integer.parseInt(id) );
+    public void delSeletedUser(String[] uids) {
+        /* delSeletedUser( String[] uids )
+        * 遍历这些用户id，逐个地删除之
+        *  */
+        if( uids!=null && uids.length>0 ){
+            for( String oneUid : uids ){ //遍历这些用户id，逐个地删除之
+                dao.delete( Integer.parseInt(oneUid) );
             }
         }
+
     }
 
     @Override
-    public PageBean<User> findUserByPage(String _currentPage, String _rows, Map<String, String[]> condition) {
-        int currentPage = Integer.parseInt(_currentPage);
-        int rows = Integer.parseInt(_rows);
+    //将所需数据【setXXX-填入】PageBean 实例对象pb中，返回给前端页面以【动态展示数据】嗷！
+    public PageBean<User> findUserByPage(String _currentPage, String _rows) {
+        //----------1.创建空的PageBean 实例对象pb
+        PageBean<User> pb = new PageBean<>();
 
-        if(currentPage <=0) {
-            currentPage = 1;
-        }
-        //1.创建空的PageBean对象
-        PageBean<User> pb = new PageBean<User>();
-        //2.设置参数
-        pb.setCurrentPage(currentPage);
-        pb.setRows(rows);
+        //先把字符串形式的数据转为 整型数据
+        int currentPage = Integer.parseInt( _currentPage );
+        int rows = Integer.parseInt( _rows );
+        //----------2.为实例对象pb【setXXX-填入数据】 当前页面currentPage、和 rows
+        pb.setCurrentPage( currentPage );
+        pb.setRows( rows );
 
-        //3.调用dao查询总记录数
-        int totalCount = dao.findTotalCount(condition);
-        pb.setTotalCount(totalCount);
-        //4.调用dao查询List集合
-        //计算开始的记录索引
-        int start = (currentPage - 1) * rows;
-        List<User> list = dao.findByPage(start,rows,condition);
-        pb.setList(list);
+        //----------3.调用dao查询totalCount总记录数，并【setXXX-填入数据】进实例对象pb中
+        int totalCount = dao.findTotalCount();
+        pb.setTotalCount( totalCount );
 
-        //5.计算总页码
-        int totalPage = (totalCount % rows)  == 0 ? totalCount/rows : (totalCount/rows) + 1;
-        pb.setTotalPage(totalPage);
+        //----------4.计算某页的开始索引start
+        //start = (currentPage-1)*rows
+        int start = (currentPage-1)*rows;
 
+        //----------5.调用dao查询list集合
+        //dao.findByPage( int start, int rows );
+        List<User> list = dao.findByPage( start, rows );
+        pb.setList( list );
 
+        //----------6.计算总页码totalPage，并【setXXX-填入数据】进实例对象pb中
+        /* private int totalPage; //总页码
+        * 总页码的计算公式：三目运算符 注意括号的包裹嗷！
+        * 【 (所有页码的总记录数totalCount % 每页显示条数rows)==0】 ? (totalCount/整除rows) : (totalCount/除以rows +1)
+        *
+        * (所有页码的总记录数totalCount)%(每页显示条数rows)==0 直接相除，余数等于0吗？
+        * 是(即余数等于0)，就用此式计算(totalCount/整除rows)
+        * 否则(余数不等于0，除不尽)，就用此式计算(totalCount/除以rows +1)
+        *  */
+        int totalPage = ( (totalCount%rows)==0 ) ? (totalCount/rows): (totalCount/rows+1);
+        pb.setTotalPage( totalPage );
+
+        //----------7.返回PageBean对象
         return pb;
     }
 }
