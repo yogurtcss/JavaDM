187c1cc38aa8e8ecc4a99bd31ba5240bf35c7f93
yogurtcss <1806631955@qq.com>
2019-12-30 10:58:39 +0800

    upload

--- Projects/Project1.1_webCase1/case1/src/pers/yo/case1/webInSrc/filter/SensitiveWordsFilter.java
+++ Projects/Project1.1_webCase1/case1/src/pers/yo/case1/webInSrc/filter/SensitiveWordsFilter.java
@@ -2,10 +2,11 @@ package pers.yo.case1.webInSrc.filter;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
-import java.io.IOException;
+import java.io.*;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -54,7 +55,7 @@ public class SensitiveWordsFilter implements Filter {
                     @Override //使用IDEA快速生成 我需要重写的方法！！
                     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
                         //---------增强getParameter()方法
-                        if( method.getName().equals("getParameter") ){
+                        if( method.getName().equals("getParameter") ){ //判断被调用的方法是否 是getParameter
                             //----------增强返回值
                             /* 先原样调用原方法，获取原本的返回值
                             * 这句话相当于调用原方法：
@@ -65,25 +66,33 @@ public class SensitiveWordsFilter implements Filter {
                             if( value!=null ){ //常规操作：判空
                                 //sensitiveList是本类的私有变量，在本类中可以通过 .运算符 直接访问
                                 for( String oneSensitiveWord : sensitiveList ){
-
+                                    if( value.contains(oneSensitiveWord) ){ //若value中包含敏感词，则和谐之
+                                        /* public String replaceAll(String regex, String replacement)
+                                        * 使用给定的 replacement 【替换】原字符串中 【与给定的正则表达式regex相匹配的】子字符串
+                                        *
+                                        * 这里的正则表达式regex是 “敏感词”oneSensitiveWord，将其替换为***
+                                        *  */
+                                        value = value.replaceAll( oneSensitiveWord, "***" );
+                                    }
                                 }
                             }
-
+                            return value;
                         }
-                        return null;
+                        //还可以判断被调用的方法名是否是 getParameterMap
+                        //还可以判断被调用的方法名是否是 getParameterValue
+
+                        //其他方法则(传入原本的形参) 原样调用；此方法所属类的实例对象是 真实对象-被代理的对象req嗷！
+                        return( method.invoke( req, args ) );
                     }
                 }
         );
 
-
-
-
         //放行前，对request对象请求消息进行【增强处理】，并传入增强后的req
+        //在这里，增强后的req为 动态代理类实例对象 proxy_req
         //执行这一句，说明放行（让下一个过滤器执行，如果没有过滤器了，就执行执行目标资源）
-        chain.doFilter(req, resp);
+        chain.doFilter( proxy_req, resp );
 
         //放行后，对response对象的响应消息进行【增强处理】
-
     }
 
     //凡是被 private 修饰的成员变量，都称为私有变量。它只允许在本类的内部通过 "点." 直接访问，任何外部类都不能访问它。
@@ -113,18 +122,55 @@ public class SensitiveWordsFilter implements Filter {
             * 在Web1项目的 web文件夹/ WEB-INF文件夹 中的 WEB-INF_C.txt，
             * (将被复制到服务器中的地址 →) 它所在服务器中的地址为：/web /WEB-INF 文件夹中
             *
-            * sensitiveWords.txt文件被我放在 src/props 文件夹下
-            * 写法为 getRealPath( "/WEB-INF/classes/ props/sensitiveWords.txt" )
+            * 我佛了，我写错地址了…
+            * sensitiveWords.txt文件被我放在 src/pers/yo/case1/props 文件夹下
+            * 【本地电脑src目录等价于 服务器中的/WEB-INF /classes】
+            * 写法为 getRealPath( "/WEB-INF/classes/ pers/yo/case1/props/sensitiveWords.txt" )
             *  */
-            String realPath = servletContext.getRealPath( "/WEB-INF/classes/props/sensitiveWords.txt" );
+            String realPath = servletContext.getRealPath( "/WEB-INF/classes/pers/yo/case1/props/sensitiveWords.txt" );
+            //---------读取文件。  ▲ 注意：原本的敏感词sensitiveWords.txt 需【事先】以UTF-8编码格式保存！！
 
+            /* BufferedReader( Reader r )
+            * 当我们创建新的 BufferedReader 对象而不显式提供编码时，Java 默认会使用哪种编码 / 字符集？
+            * BufferedReader不执行任何解码。它是另一个 Reader ... 的包装，它可以解码也可以不解码。
+            * 可理解为：BufferedReader跟随FileReader进行解码！！
+            * FileReader使用 JVM 的默认字符编码 GBK（由 Charset.defaultCharset()返回）【进行解码】
+            *
+            * FileReader 竟然没有可以指定字符编码的构造方法。
+            * 而且仅仅是简单地从 父类InputStreamReader(转换流，字节流通向字符流的桥梁) 继承，并没有重写或扩展任何方法。
+            * 这可能是历史上最吝啬的子类，完全就是啃老族。
+            *
+            * 父类InputStreamReader：是字节流与字符流之间的桥梁，
+            *   能将字节流输出为字符流，并且【能为字节流指定字符集】，可输出一个个的字符；
+            *
+            * 在使用BufferedReader( Reader r )构造方法，传入形参Reader时：
+            *   1.若已知读取的文件是GBK编码，可以 new FileReader( GBK编码的文件 ) 直接传入:
+            *    - new BufferedReader( new FileReader(GBK编码的文件) );
+            *   2.若读取文件不是GBK编码，不使用 FileReader 了！！
+            *   则需要用 父类InputStreamReader【代替FileReader】，传入 指定字节输入流的编码！
+            *    - InputStreamReader isr_utf8 = new InputStreamReader( new FileInputStream文件字节输入流( UTF8编码的文件 ), "UTF-8"指定字符集 )
+            *    - new BufferedReader( isr_utf8 );
+            *  */
 
+            /* InputStreamReader isr_utf8 = new InputStreamReader( new FileInputStream(readPath), "UTF-8" );
+            * 注意，第二个参数字符集 "UTF-8"可以替换为 变量StandardCharsets.UTF_8
+            *  */
+            InputStreamReader isr_utf8 = new InputStreamReader( new FileInputStream(realPath), StandardCharsets.UTF_8 );
+            //因为原输入文件是UTF-8编码，不能直接用 FileReader读入！！
+            // BufferedReader br = new BufferedReader( new FileReader(realPath) );
+            BufferedReader br = new BufferedReader( isr_utf8 );
 
+            //---------将文件中的每一行数据添加到list中
+            String line = null;
+            while( (line=br.readLine())!=null ){ //未读到文件末尾
+                sensitiveList.add(line); //私有变量sensitiveList只允许在本类中可任意调用嗷！
+            }
+            br.close(); //释放资源
+            System.out.println( sensitiveList );
 
         }catch( Exception e ){
             e.printStackTrace();
         }
-
     }
 
 
