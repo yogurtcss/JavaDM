14cc460b1c17416445f0daaf485e32d566eda580
yogurtcss <1806631955@qq.com>
2020-01-03 20:31:38 +0800

    upload

--- Projects/Project1_web/Web1/src/pers/yo/study/Demo08_Jedis/example/service/ProvinceService.java
+++ Projects/Project1_web/Web1/src/pers/yo/study/Demo08_Jedis/example/service/ProvinceService.java
@@ -7,5 +7,5 @@ import java.util.List;
 public interface ProvinceService {
     public abstract List<Province> findAll();
 
-    //public abstract String findAllJson();  //redis缓存优化的方法
+    public abstract String findAllJson();  //redis缓存优化的方法
 }
