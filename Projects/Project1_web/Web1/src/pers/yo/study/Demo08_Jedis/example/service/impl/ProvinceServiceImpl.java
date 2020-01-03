88e4ec6900a0e5d10a583c5b7e5ec54791791f55
yogurtcss <1806631955@qq.com>
2020-01-03 21:01:56 +0800

    upload

--- Projects/Project1_web/Web1/src/pers/yo/study/Demo08_Jedis/example/service/impl/ProvinceServiceImpl.java
+++ Projects/Project1_web/Web1/src/pers/yo/study/Demo08_Jedis/example/service/impl/ProvinceServiceImpl.java
@@ -1,9 +1,13 @@
 package pers.yo.study.Demo08_Jedis.example.service.impl;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import pers.yo.study.Demo08_Jedis.example.dao.ProvinceDao;
 import pers.yo.study.Demo08_Jedis.example.dao.impl.ProvinceDaoImpl;
 import pers.yo.study.Demo08_Jedis.example.domain.Province;
 import pers.yo.study.Demo08_Jedis.example.service.ProvinceService;
+import pers.yo.study.Demo08_Jedis.example.util.JedisPoolUtils;
+import redis.clients.jedis.Jedis;
 
 import java.util.List;
 
@@ -28,5 +32,43 @@ public class ProvinceServiceImpl implements ProvinceService {
     @Override //IDEA快速生成 需要重写的方法
     public List<Province> findAll() {
         return( dao.findAll() );
+    }
+
+    @Override
+    public String findAllJson() { //redis优化缓存的方法
+
+        //-----------------1.先从redis数据库中查询数据
+        //-----1.1 获取一个jedis连接对象
+        Jedis jedis = JedisPoolUtils.getJedis();
+        //尝试性地：在redis数据库中查看有无 键名为province 对应的值？
+        String province_json = jedis.get( "province" ); //若此键没有值，我仍以键名为"province"，存新的值进去嗷
+
+        //-----------------2.判断redis数据库中的 province_json是否为空
+        if( province_json==null || province_json.length()==0 ){
+            /* 若此键的值是空的，说明redis数据库中没有缓存
+            * 那我就从真正的 mysql数据库中，查得数据data
+            * 并将此数据data 对应着键名"province" 存入 redis数据库中，
+            * 这样 数据data就有缓存了！
+            *  */
+            System.out.println( "redis数据库中没有此数据，正从MySQL中查询数据..." );
+            //------2.1这里是service层，往下调用最底层的dao！！
+            List<Province> rst = dao.findAll();
+
+            //------2.2将用MySQL查询的数据转为JSON格式，然后存入 redis数据库中
+            ObjectMapper om = new ObjectMapper(); //Jackson核心对象
+            try { //使用 IDEA 快速帮我捕获异常！
+                province_json = om.writeValueAsString( rst );
+            } catch (JsonProcessingException e) {
+                e.printStackTrace();
+            }
+            //------2.3将此JSON数据 以键名province存入redis数据库中
+            jedis.set( "province", province_json );
+            //------2.4释放资源，把连接对象给回连接池
+            jedis.close();
+        }else{
+            System.out.println( "redis中有缓存数据，查询缓存嗷！" );
+        }
+
+        return province_json;
     }
 }
