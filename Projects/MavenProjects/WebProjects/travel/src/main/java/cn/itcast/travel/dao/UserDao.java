4e840187580ef8dbb420257ba73614e0e644f9f9
yogurtcss <1806631955@qq.com>
2020-01-06 20:15:51 +0800

    upload

--- Projects/MavenProjects/WebProjects/travel/src/main/java/cn/itcast/travel/dao/UserDao.java
+++ Projects/MavenProjects/WebProjects/travel/src/main/java/cn/itcast/travel/dao/UserDao.java
@@ -8,5 +8,5 @@ public interface UserDao {
     public abstract void save( User user ); //保存用户
     public abstract User findByCode( String code ); //根据激活码查找用户, ——下一步是修改此用户的激活状态为Y
     public abstract void updateStatus( User user ); //修改此用户的激活状态为Y
-    public abstract User findByUserAndPassword( String username, String password ); //通过用户名和密码 找到某用户
+    public abstract User findByUsernameAndPassword( String username, String password ); //通过用户名和密码 找到某用户
 }
