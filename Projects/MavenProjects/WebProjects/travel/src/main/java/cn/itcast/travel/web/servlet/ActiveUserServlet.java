fc2b3cb9542338912cd56f24e59d55170f618364
yogurtcss <1806631955@qq.com>
2020-01-06 23:18:29 +0800

    upload

--- Projects/MavenProjects/WebProjects/travel/src/main/java/cn/itcast/travel/web/servlet/ActiveUserServlet.java
+++ Projects/MavenProjects/WebProjects/travel/src/main/java/cn/itcast/travel/web/servlet/ActiveUserServlet.java
@@ -29,6 +29,21 @@ public class ActiveUserServlet extends HttpServlet {
             // 向浏览器返回数据——HTML文本(如纯文字 或a标签、div标签等) 直接显示在html页面上的！
             String resMsg = null; //初始化：返回的字符串
             if( flag==true ){ //激活标志为true
+                /* 【坑】注意这里 login.html的地址：
+                * 正确的访问网址名应该是 localhost:8080/travel/login.html
+                * 这里写的是相对位置 省略了前面的服务器地址localhost:8080/travel/ 了(末尾已经带斜杠了)
+                *
+                * Maven中：src/main/webapp 这个目录存放 Web配置文件，JSP 和静态文件
+                * src/main/webapp 相当于IDEA中 Java WEB项目的【web目录】(下边放 WEB-INF 目录)
+                * 访问 某个静态资源页面时，总是到 此webapp目录(类比 Java WEB项目的web目录)下去找此静态资源！！
+                *
+                * 而我已知在IDEA中 Java WEB项目中 不带前缀地访问项目中的某个网页
+                * 默认就是带上前缀 localhost:8080/项目名/ 了
+                *
+                * 在maven中，这里也是同理的！不带前缀地访问项目中的某个网页
+                * 默认就是带上前缀 localhost:8080/项目名/ 了
+                *
+                *  */
                 resMsg = "激活成功，请<a href='login.html'>登录</a>";
             }else{
                 resMsg = "<h1>激活失败，请联系管理员！</h1>";
