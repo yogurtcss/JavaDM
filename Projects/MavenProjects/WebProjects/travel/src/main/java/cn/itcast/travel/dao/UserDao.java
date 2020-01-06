fbdac6c9a5d2ade6b3656e02a8eef348d9479fdf
yogurtcss <1806631955@qq.com>
2020-01-06 20:07:20 +0800

    upload

--- Projects/MavenProjects/WebProjects/travel/src/main/java/cn/itcast/travel/dao/UserDao.java
+++ Projects/MavenProjects/WebProjects/travel/src/main/java/cn/itcast/travel/dao/UserDao.java
@@ -4,6 +4,9 @@ import cn.itcast.travel.domain.User;
 
 public interface UserDao {
     //接口中全是 public abstract方法
-    public abstract User findByUsername( String username );
-    public abstract void save( User user );
+    public abstract User findByUsername( String username ); //根据用户查找用户
+    public abstract void save( User user ); //保存用户
+    public abstract User findByCode( String code ); //根据激活码查找用户, ——下一步是修改此用户的激活状态为Y
+    public abstract void updateStatus( User user ); //修改此用户的激活状态为Y
+    public abstract User findByUserAndPassword( String username, String password ); //通过用户名和密码 找到某用户
 }
