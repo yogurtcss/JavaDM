d70bffa3431ba9fe87aea03a72cc6293ff4a530e
yogurtcss <1806631955@qq.com>
2020-01-06 20:15:40 +0800

    upload

--- Projects/MavenProjects/WebProjects/travel/src/main/java/cn/itcast/travel/service/impl/UserServiceImpl.java
+++ Projects/MavenProjects/WebProjects/travel/src/main/java/cn/itcast/travel/service/impl/UserServiceImpl.java
@@ -4,27 +4,68 @@ import cn.itcast.travel.dao.UserDao;
 import cn.itcast.travel.dao.impl.UserDaoImpl;
 import cn.itcast.travel.domain.User;
 import cn.itcast.travel.service.UserService;
+import cn.itcast.travel.util.MailUtils;
+import cn.itcast.travel.util.UuidUtil;
 
 public class UserServiceImpl implements UserService {
-    UserDao dao = new UserDaoImpl(); //接口回调，向上转型
+    private UserDao dao = new UserDaoImpl(); //接口回调，向上转型
 
     @Override //使用IDEA快速生成需重写的方法
     public boolean regist(User user) {
-        /* 调用dao，根据用户名查询用户
+        /* ---1.调用dao，根据用户名查询用户
         * userFromSQL 可能存在的用户
         *  */
-        User userFromSQL = dao.findByUsername( user.getUsername() );
+        System.out.println( "用户名是："+user.getUsername() );
+        /* 往后涉及到 从数据库中查询而得的“对象”rst 时，养成好习惯：
+        * 先实例化一个对象A，初始化赋值为 null
+        * 然后在 try...catch...中 把 从数据库中查询而得的“对象”rst 赋值给 A
+        * 这样就安全了！
+        *  */
+        User userFromSQL = null; //先实例化一个对象A，初始化赋值为 null
+        try{
+            //然后在 try...catch...中 把 从数据库中查询而得的“对象”rst 赋值给 A
+            userFromSQL = dao.findByUsername( user.getUsername() );
+        }catch( Exception e ){
+            e.printStackTrace();
+        }
 
-        return false;
+        if( userFromSQL!=null ){
+            return false; //用户名存在，注册失败
+        }
+
+        //---2.用户名不存在时：保存用户信息
+        //2.1 设置激活码：利用UUID工具类 生成UUID全局唯一字符串
+        user.setCode( UuidUtil.getUuid() );
+        //2.2 设置激活状态
+        user.setStatus( "N" );
+        //正式保存
+        dao.save( user );
+
+        //---3.向用户邮箱发送激活邮件
+        // <a href='http://localhost/travel/activeUserServlet?code=XXXYYY'> 点击激活[来自黑马旅游网] </a>
+        //编写邮件正文: 把用户的激活码code, [通过请求参数code] 传给activeUserServlet
+        String content = "<a href='http://localhost/travel/activeUserServlet?code="+user.getCode()+"'>点击激活[来自黑马旅游网]</a>";
+        //正式发送邮件
+        MailUtils.sendMail( user.getEmail(), content, "激活邮件" );
+        //返回注册标志为true
+        return true;
     }
 
     @Override
-    public boolean active(String code) {
+    public boolean active(String code) { //激活用户: 修改用户的状态status为 Y
+        //---1.根据激活码查询用户对象
+        User user = dao.findByCode( code );
+        if( user!=null ){
+            //---2.调用dao层，修改用户的激活状态为Y
+            dao.updateStatus( user );
+            return true;
+        }
+        //如果等于null，说明数据库中没有此用户，直接返回false
         return false;
     }
 
     @Override
     public User login(User user) {
-        return null;
+        return( dao.findByUsernameAndPassword( user.getUsername(), user.getPassword() ) );
     }
 }
