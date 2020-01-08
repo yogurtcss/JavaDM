b8dc294adb9b89efd0ebe0db9242b61e8c48f6d0
yogurtcss <1806631955@qq.com>
2020-01-08 22:06:47 +0800

    upload

--- Projects/MavenProjects/WebProjects/travel/src/main/java/cn/itcast/travel/dao/CategoryDao.java
+++ Projects/MavenProjects/WebProjects/travel/src/main/java/cn/itcast/travel/dao/CategoryDao.java
@@ -5,5 +5,5 @@ import cn.itcast.travel.domain.Category;
 import java.util.List;
 
 public interface CategoryDao {
-    public List<Category> findAll(); //查询所有的条目 即select * from XXX
+    public abstract List<Category> findAll(); //查询所有的条目 即select * from XXX
 }
