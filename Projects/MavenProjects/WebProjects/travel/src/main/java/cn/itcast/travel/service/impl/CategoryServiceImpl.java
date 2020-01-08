9f9b19401b27759e99ffbc3b2eae9380935eddec
yogurtcss <1806631955@qq.com>
2020-01-08 22:14:42 +0800

    upload

--- Projects/MavenProjects/WebProjects/travel/src/main/java/cn/itcast/travel/service/impl/CategoryServiceImpl.java
+++ Projects/MavenProjects/WebProjects/travel/src/main/java/cn/itcast/travel/service/impl/CategoryServiceImpl.java
@@ -3,11 +3,12 @@ package cn.itcast.travel.service.impl;
 import cn.itcast.travel.dao.CategoryDao;
 import cn.itcast.travel.dao.impl.CategoryDaoImpl;
 import cn.itcast.travel.domain.Category;
+import cn.itcast.travel.service.CategoryService;
 
 import java.util.List;
 
-public class CategoryServiceImpl implements CategoryDao {
-    private CategoryDao dao = new CategoryServiceImpl(); //接口回调，向上转型
+public class CategoryServiceImpl implements CategoryService {
+    private CategoryDao dao = new CategoryDaoImpl(); //接口回调，向上转型
 
     @Override //使用IDEA快速生成需重写的方法
     public List<Category> findAll() {
