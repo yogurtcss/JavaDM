859407b58cc02b52aba2efbfc5d861cba372162a
yogurtcss <1806631955@qq.com>
2020-01-11 17:03:51 +0800

    upload

--- Projects/MavenProjects/WebProjects/travel/src/main/java/cn/itcast/travel/dao/impl/RouteDaoImpl.java
+++ Projects/MavenProjects/WebProjects/travel/src/main/java/cn/itcast/travel/dao/impl/RouteDaoImpl.java
@@ -23,47 +23,59 @@ public class RouteDaoImpl implements RouteDao {
 
         List<Object> params = new ArrayList<>(); //条件们，注意List的泛型是Object！
         if( cid!=0 ){
-            sb.append( "   and cid=?   " ); //注意拼接处的空格！
+            sb.append( " and cid=? " ); //注意拼接处的空格！
             params.add( cid ); //添加 ?问号对应的值
         }
-        if( rname!=null && rname.length()>0 ){ //模糊查询 like关键字
-            sb.append( "   and rname like ?   " ); //注意拼接处的空格！
+        /* 2020-01-11 16:45:34 排了整整大半天的BUG
+        * 【问题描述】当不传入rname时(即rname为null时)。
+        * 在前端页面 route_list.html 中，不能进行翻页！
+        * 【解决】在关于rname的判空操作中，加上一个 “且”的条件！！ ：
+        * !rname.equals("null") 要求 rname字符串的内容 不为 "null" ！！
+        *
+        * 前端代码中，只需检查一下 关于rname变量的 <li />标签有无写错即可！！
+        * <li />标签的拼串处，很恶心！
+        *  */
+        if( rname!=null && rname.length()>0 && !rname.equals("null")){ //模糊查询 like关键字
+            sb.append( " and rname like ? " ); //注意拼接处的空格！
             params.add( "%"+rname+"%" ); //添加 ?问号对应的值，模糊查询，要加百分号！
         }
         sql = sb.toString(); //将StringBuilder还原为字符串嗷！
-
-
-        int rst = 0;
-        try{ //数据库的赋值，全在try中进行嗷！
-            rst = template.queryForObject( sql, Integer.class, params.toArray() );
-        }catch( Exception e ){
-            //不处理
-        }
-
-        return rst;
+        System.out.println( "rname是空的吗？"+rname );
+        System.out.println( "sql语句为："+sql );
+        return template.queryForObject( sql, Integer.class, params.toArray() );
     }
 
     @Override //int pageSize; //每页显示的条数
     public List<Route> findByPage(int cid, int start, int pageSize, String rname) {
         //String sql = "select * from tab_route where cid=? limit ?,?";
-        String sql = "select * from tab_route where 1=1   ";
+        String sql = "select * from tab_route where 1=1 ";
         StringBuilder sb = new StringBuilder( sql );
 
         List<Object> params = new ArrayList<>(); //条件们
         if( cid!=0 ){
-            sb.append( "  and cid = ?  " );
+            sb.append( " and cid = ? " );
             params.add( cid ); //添加 ? 对应的值
         }
-        if( rname!=null && rname.length()>0 ){
-            sb.append( "   and rname like ?   " ); //模糊查询
+        /* 2020-01-11 16:45:34 排了整整大半天的BUG
+        * 【问题描述】当不传入rname时(即rname为null时)。
+        * 在前端页面 route_list.html 中，不能进行翻页！
+        * 【解决】在关于rname的判空操作中，加上一个 “且”的条件！！ ：
+        * !rname.equals("null") 要求 rname字符串的内容 不为 "null" ！！
+        *
+        * 前端代码中，只需检查一下 关于rname变量的 <li />标签有无写错即可！！
+        * <li />标签的拼串处，很恶心！
+        *  */
+
+        if( rname!=null && rname.length()>0 && !rname.equals("null") ){
+            sb.append( " and rname like ? " ); //模糊查询
             params.add( "%"+rname+"%" ); //模糊查询 加百分号！
         }
-        sb.append( "   limit ?,?   " ); //分页条件
+        sb.append( " limit ?, ? " ); //分页条件
         sql = sb.toString(); //转为字符串
 
         params.add( start );
         params.add( pageSize );
-
+        System.out.println( "sql语句为："+sql );
         return( template.query(
                 sql,
                 new BeanPropertyRowMapper<Route>(Route.class),
