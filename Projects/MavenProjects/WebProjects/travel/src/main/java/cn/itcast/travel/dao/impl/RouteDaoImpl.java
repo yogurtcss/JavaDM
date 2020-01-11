a8dc846173280cf4adb107ae46cb32fd26ed97a9
yogurtcss <1806631955@qq.com>
2020-01-11 17:07:47 +0800

    upload

--- Projects/MavenProjects/WebProjects/travel/src/main/java/cn/itcast/travel/dao/impl/RouteDaoImpl.java
+++ Projects/MavenProjects/WebProjects/travel/src/main/java/cn/itcast/travel/dao/impl/RouteDaoImpl.java
@@ -28,9 +28,14 @@ public class RouteDaoImpl implements RouteDao {
         }
         /* 2020-01-11 16:45:34 排了整整大半天的BUG
         * 【问题描述】当不传入rname时(即rname为null时)。
-        * 在前端页面 route_list.html 中，不能进行翻页！
+        * 在前端页面 route_list.html 中，不能进行翻页！(第2、3、以后的页为空）
         * 【解决】在关于rname的判空操作中，加上一个 “且”的条件！！ ：
         * !rname.equals("null") 要求 rname字符串的内容 不为 "null" ！！
+        *
+        * 若不检查 rname字符串的内容 是否为 "null"，
+        * 则动态添加的sql语句中，就会把 and rname like ? 给加上；
+        * 而 rname为"null" --在数据库中找不到值！
+        * 所以 在前端页面 route_list.html 中，就不能进行翻页！(第2、3、以后的页为空）
         *
         * 前端代码中，只需检查一下 关于rname变量的 <li />标签有无写错即可！！
         * <li />标签的拼串处，很恶心！
@@ -58,14 +63,18 @@ public class RouteDaoImpl implements RouteDao {
         }
         /* 2020-01-11 16:45:34 排了整整大半天的BUG
         * 【问题描述】当不传入rname时(即rname为null时)。
-        * 在前端页面 route_list.html 中，不能进行翻页！
+        * 在前端页面 route_list.html 中，不能进行翻页！(第2、3、以后的页为空）
         * 【解决】在关于rname的判空操作中，加上一个 “且”的条件！！ ：
         * !rname.equals("null") 要求 rname字符串的内容 不为 "null" ！！
+        *
+        * 若不检查 rname字符串的内容 是否为 "null"，
+        * 则动态添加的sql语句中，就会把 and rname like ? 给加上；
+        * 而 rname为"null" --在数据库中找不到值！
+        * 所以 在前端页面 route_list.html 中，就不能进行翻页！(第2、3、以后的页为空）
         *
         * 前端代码中，只需检查一下 关于rname变量的 <li />标签有无写错即可！！
         * <li />标签的拼串处，很恶心！
         *  */
-
         if( rname!=null && rname.length()>0 && !rname.equals("null") ){
             sb.append( " and rname like ? " ); //模糊查询
             params.add( "%"+rname+"%" ); //模糊查询 加百分号！
