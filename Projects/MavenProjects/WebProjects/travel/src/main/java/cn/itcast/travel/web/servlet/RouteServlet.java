ba44c509bdf63d0eae262e41b543d89ca941bc3f
yogurtcss <1806631955@qq.com>
2020-01-11 10:39:13 +0800

    upload

--- Projects/MavenProjects/WebProjects/travel/src/main/java/cn/itcast/travel/web/servlet/RouteServlet.java
+++ Projects/MavenProjects/WebProjects/travel/src/main/java/cn/itcast/travel/web/servlet/RouteServlet.java
@@ -22,6 +22,8 @@ public class RouteServlet extends BaseServlet { //继承基类BaseServlet
         String currentPageStr = request.getParameter( "currentPage" );
         String pageSizeStr = request.getParameter( "pageSize" ); //实际在route_list.html的前台代码中，没有传递pageSize参数！
 
+        String rname = request.getParameter( "rname" ); //在前台已进行URI编码，后台接收到的是正常的中文字符串
+        rname = new String(rname.getBytes("iso-8859-1"),"utf-8");
 
         //---2.前端传过来的形参都是字符串型的，需处理接收的字符串参数，做强制类型转换嗷！
         //------强制类型转换cid
@@ -46,10 +48,28 @@ public class RouteServlet extends BaseServlet { //继承基类BaseServlet
         }
 
         //---3.调用service层，查询出pageBean对象
-        PageBean<Route> pb_final = service.pageQuery( cid, currentPage, pageSize );
-
+        PageBean<Route> pb_final = service.pageQuery( cid, currentPage, pageSize, rname );
+        System.out.println( "pb_final是空的吗"+pb_final==null );
         //---4.将数据转为JSON对象，并填入 流中
         writeValueToResponse( response, pb_final ); //BaseServlet中，自定义的方法嗷！
+    }
+
+    /* 2020-01-11 09:57:29
+    * 修饰符改为 public，只改变方法名，其余不变
+    *  */
+    public void findOne(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        /* 前情回顾：在route_list.html 中：
+        * <p><a href="route_detail.html?rid='   +route.rid+    '">查看详情</a></p>
+        * 传输了 route.rid 这个请求参数
+        *
+        * RouteService这个service层中，实现了 findOne方法 --查询某个商品的详情信息
+        * 在这里直接查询出来，回写给前端
+        *  */
+        String rid = request.getParameter( "rid" );
+        //---调用service层
+        Route route_final = service.findOne( rid );
+        //---在这里直接查询出来，回写给前端
+        writeValueToResponse( response, route_final );
     }
 
 }
