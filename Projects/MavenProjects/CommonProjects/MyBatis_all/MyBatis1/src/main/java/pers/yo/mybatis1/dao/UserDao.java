817243d0f79ca335be6bc2f9754c96cc1f86f6d6
yogurtcss <1806631955@qq.com>
2020-01-12 20:52:28 +0800

    upload

--- Projects/MavenProjects/CommonProjects/MyBatis_all/MyBatis1/src/main/java/pers/yo/mybatis1/dao/UserDao.java
+++ Projects/MavenProjects/CommonProjects/MyBatis_all/MyBatis1/src/main/java/pers/yo/mybatis1/dao/UserDao.java
@@ -1,5 +1,6 @@
 package pers.yo.mybatis1.dao;
 
+import org.apache.ibatis.annotations.Select;
 import pers.yo.mybatis1.domain.User;
 
 import java.util.List;
@@ -7,5 +8,6 @@ import java.util.List;
 //用户的持久层接口 UserDao.java
 public interface UserDao {
     //查询所有数据
+    @Select( "select * from user" ) //在持久层接口中，对接口中的某个方法添加注解
     public abstract List<User> findAll();
 }
