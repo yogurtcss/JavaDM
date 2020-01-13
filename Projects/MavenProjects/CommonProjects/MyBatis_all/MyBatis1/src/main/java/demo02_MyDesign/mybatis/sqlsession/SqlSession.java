b83d958cc64a657bb4dd87e82ed511b4a5598138
yogurtcss <1806631955@qq.com>
2020-01-13 20:13:33 +0800

    upload

--- Projects/MavenProjects/CommonProjects/MyBatis_all/MyBatis1/src/main/java/demo02_MyDesign/mybatis/sqlsession/SqlSession.java
+++ Projects/MavenProjects/CommonProjects/MyBatis_all/MyBatis1/src/main/java/demo02_MyDesign/mybatis/sqlsession/SqlSession.java
@@ -8,6 +8,13 @@ public interface SqlSession {
      * @param <T>
      * @return
      */
+    /* 2020-01-13 20:10:12
+    * 返回值类型是 泛型T，为什么要写成 <T> T ？
+    * 解释：泛型要先声明后使用，声明在返回值之前
+    * <T>是“先声明”，声明在返回值之前；
+    * 返回值 T 就是“使用”了
+    * 所以 <T>声明 就在 返回值T 之前了
+    *  */
     public abstract <T> T getMapper( Class<T> daoInterfaceClass );
 
     public abstract void close(); //释放资源
