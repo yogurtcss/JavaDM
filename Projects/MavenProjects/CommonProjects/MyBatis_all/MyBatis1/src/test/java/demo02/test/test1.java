99753ee3f60c78a9fdad49d47d1bbdfe6b6b9395
yogurtcss <1806631955@qq.com>
2020-01-13 21:19:53 +0800

    upload

--- Projects/MavenProjects/CommonProjects/MyBatis_all/MyBatis1/src/test/java/demo02/test/test1.java
+++ Projects/MavenProjects/CommonProjects/MyBatis_all/MyBatis1/src/test/java/demo02/test/test1.java
@@ -14,7 +14,22 @@ import java.util.List;
 public class test1 {
     public static void main(String[] args) throws IOException {
         //1.读取配置文件
-        InputStream is = Resources.getResourceAsStream( "SqlMapConfig.xml" );
+        /* 2020-01-13 21:19:43
+        * 读取配置文件时：默认是从 resources的总目录下 进行读取的
+        * 若我把SqlMapConfig_demo2.xml 放在 resources总目录/ props文件夹/ SqlMapConfig_xml文件夹  下
+        * 看看能否读取到此配置文件SqlMapConfig_demo2.xml
+        *     ——【已验证】，可以正确读取此SqlMapConfig_demo2.xml配置文件
+        *
+        * 此时的相对路径为：props/SqlMapConfig_xml/SqlMapConfig_demo2.xml
+        * 注意，开头没有斜杠！！
+        *  */
+
+        //把SqlMapConfig.xml 放在 resources总目录下时，直接写文件名即可
+        //InputStream is = Resources.getResourceAsStream( "SqlMapConfig.xml" );
+
+        //相对路径，是以 resources总目录 为基准，进行寻找配置文件嗷
+        //注意，开头没有斜杠！！
+        InputStream is = Resources.getResourceAsStream( "props/SqlMapConfig_xml/SqlMapConfig_demo2.xml" );
         //2.创建 SqlSessionFactory 的构建者builder，等待创建真正的工厂
         SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
         //3.真正创建工厂
