1bf6819bdcba4836a3e156e78c53d9c72b5f1f08
yogurtcss <1806631955@qq.com>
2020-01-15 10:08:26 +0800

    upload

--- Projects/MavenProjects/CommonProjects/MyBatis_all/MyBatis1/src/main/java/demo02_MyDesign/mybatis/io/Resources.java
+++ Projects/MavenProjects/CommonProjects/MyBatis_all/MyBatis1/src/main/java/demo02_MyDesign/mybatis/io/Resources.java
@@ -5,7 +5,7 @@ import java.io.InputStream;
 //此Resources类的功能：使用类加载器，读取配置文件
 
 /* 2020-01-15 10:06:29
-Resources   —— org.apache.ibatis.io.Resources
+Resources工具类(直接调用其中的静态方法)   —— org.apache.ibatis.io.Resources
 1.从类路径加载 SQL Map 配置文件（如 sqlMap-config.xml）。
 2.从类路径加载 DAO Manager 配置文件（如 dao.xml）。
 3.从类路径加载各种.properties 文件。
