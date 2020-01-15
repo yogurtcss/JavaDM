9762d1591fde0207c7be38fe6583ab1e03b7dbab
yogurtcss <1806631955@qq.com>
2020-01-15 09:56:04 +0800

    upload

---
+++ Projects/MavenProjects/CommonProjects/MyBatis_all/MyBatis1/src/main/java/demo04_DynamicSQL/dao/UserDao.java
@@ -0,0 +1,18 @@
+package demo04_DynamicSQL.dao;
+
+import demo04_DynamicSQL.domain.QueryVo;
+import demo04_DynamicSQL.domain.User;
+
+import java.util.List;
+
+public interface UserDao {
+    public abstract List<User> findAll();
+    public abstract User findById( Integer id );
+    public abstract List<User> findByName( String username ); //根据名称模糊查询用户信息
+    public abstract List<User> findUserByVo( QueryVo vo ); //根据queryVo中的条件查询用户
+
+    //根据queryVo中的条件查询用户：user 查询的条件：有可能有用户名，有可能有性别，也有可能有地址，还有可能是都有
+    public abstract List<User> findUserByCondition( QueryVo vo );
+
+    public abstract List<User> findUserInIds( QueryVo vo ); //根据queryvo中提供的id集合，查询用户信息
+}
