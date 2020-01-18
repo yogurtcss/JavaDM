e95c3017bc09f7775272a84d1907adea4e3e528f
yogurtcss <1806631955@qq.com>
2020-01-18 17:59:37 +0800

    upload

--- Projects/MavenProjects/CommonProjects/Spring_all/Factory1/src/main/java/demo1/service/impl/AccountServiceImpl.java
+++ Projects/MavenProjects/CommonProjects/Spring_all/Factory1/src/main/java/demo1/service/impl/AccountServiceImpl.java
@@ -5,27 +5,31 @@ import demo1.dao.impl.AccountDaoImpl;
 import demo1.factory.BeanFactory;
 import demo1.service.AccountService;
 
-import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.Method;
-import java.lang.reflect.Proxy;
-
 public class AccountServiceImpl implements AccountService {
 
     //private AccountDao dao = new AccountDaoImpl();  //这样做是合法的
-    private AccountDao dao = (AccountDao)BeanFactory.getBean( "accountDao" );  //报错，空指针异常，说明只能通过 动态代理创建接口实现类
+    //private AccountDao dao = (AccountDao)BeanFactory.getBean( "accountDao" );  //报错，空指针异常
     //private AccountDao dao;
 
+    private AccountDao dao = (AccountDao) BeanFactory.getBean("accountDao");
+    /* 2020-01-18 17:23:16
+    * 之前的例子：
+    * 在 UserDaoImpl中：private JdbcTemplate template = new JdbcTemplate(JDBCUtils.getDataSource());
+    * 在 UserServiceImpl中：private UserDao dao = new UserDaoImpl();
+    * ∴ 用new关键字 正射出来的 private私有变量 是在本类中正常使用的
+    *
+    * 而在这里，用 工厂类BeanFactory 反射出来的 private私有变量，在本类中是 null……
+    *
+    *  */
+
+    //private AccountDao dao = (AccountDao)Class.forName( "demo1.dao.impl.AccountDaoImpl" ).newInstance();
+    public AccountServiceImpl() throws ClassNotFoundException, IllegalAccessException, InstantiationException {
+    }
+
+    @Override
     public void saveAccount() {
         //dao = (AccountDao)BeanFactory.getBean( "accountDao" );
-        System.out.println( "我是真正saveAccount()中的方法！" );
+        System.out.println( dao );
         dao.saveAccount();
     }
-
-    public void test(){
-        //dao = (AccountDao)BeanFactory.getBean("accountDao");
-        System.out.println( "------------------" );
-        System.out.println( "我是test()中的方法！" );
-        dao.saveAccount();
-    }
-
 }
