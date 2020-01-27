4de10291a9c9eaa8f5ea038c22c5bf05e9f103ff
yogurtcss <1806631955@qq.com>
2020-01-27 21:47:34 +0800

    upload

---
+++ Projects/MavenProjects/WebProjects/SpringMVC1/src/main/java/demo1/domain/User.java
@@ -0,0 +1,43 @@
+package demo1.domain;
+
+import java.util.Date;
+
+public class User {
+    private String uname;
+    private Integer age;
+
+    private Date date;
+
+    public String getUname() {
+        return uname;
+    }
+
+    public void setUname(String uname) {
+        this.uname = uname;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "uname='" + uname + '\'' +
+                ", age=" + age +
+                ", date=" + date +
+                '}';
+    }
+}
