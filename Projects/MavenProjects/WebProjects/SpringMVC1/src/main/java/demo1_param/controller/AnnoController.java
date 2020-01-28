81732366032005cb36bb5088864e9460def66c30
yogurtcss <1806631955@qq.com>
2020-01-28 12:34:40 +0800

    upload

--- Projects/MavenProjects/WebProjects/SpringMVC1/src/main/java/demo1_param/controller/AnnoController.java
+++ Projects/MavenProjects/WebProjects/SpringMVC1/src/main/java/demo1_param/controller/AnnoController.java
@@ -1,10 +1,11 @@
 package demo1_param.controller;
 
+import demo1_param.domain.User;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+import java.util.Map;
 
 @Controller
 @RequestMapping( "/anno" )
@@ -16,6 +17,7 @@ public class AnnoController {
 
     当 后台方法中的形参名 要与 传递参数的键名 不一致时：
     把注解 @RequestParam( name="前台传递参数键名" ) 加在后台方法的形参名上
+    注意：@RequestParam 的name属性等价于value属性，而只写value属性时，可以省略 value= ，直接写 "前台传递参数键名" 即可
     *  */
     @RequestMapping( "/testRequestParam" )
     public String testRequestParam( @RequestParam(name="name") String username ){
@@ -46,6 +48,49 @@ public class AnnoController {
     @RequestMapping( "/testPathVariable/{sid}" ) //@PathVariable(name="指定占位符！")
     public String testPathVariable( @PathVariable(name="sid") String XXXXXXXYYYYYYYYYYid ){ //后台方法的形参名可任意写！
         System.out.println( "通过占位符获取参数sid：(后台方法的形参名可任意写！)"+XXXXXXXYYYYYYYYYYid );
+        return "success";
+    }
+
+    @ModelAttribute //当已知返回值类型时，@ModelAttribute注解加在 有返回值类型的方法上
+    //注意：@RequestParam 的name属性等价于value属性，而只写value属性时，可以省略 value= ，直接写 "前台传递参数键名" 即可
+    public User populateUser( @RequestParam(name="username") String username ){ //populate 填充
+        //模拟：根据前台传参键名username，从数据库中查询出完整的user对象
+        User user = new User();
+        user.setUname("哈哈");
+        user.setAge(20);
+        //---以上是用户提交的数据！
+        user.setDate( new Date() ); //填充populate、补全 这个缺失的date数据
+        return user;
+    }
+
+//    @RequestMapping( "/testModelAttribute" )
+//    //这里的user不用加@RequestParam注解，因为前台传参键名没有传user对象过来啊！
+//    public String testModelAttribute( User user ){ //等待 @ModelAttribute注解预处理后的数据，填进来user
+//        System.out.println( user );
+//        return "success";
+//    }
+
+
+    @ModelAttribute ////当未知返回值类型时，@ModelAttribute注解加在 无返回值类型的方法上
+    public void populateUser2( @RequestParam(name="username")String username, Map<String,User> map ){
+        //模拟：根据前台传参键名username，从数据库中查询出完整的user对象
+        User user = new User();
+        //---假设用户在前台提交的数据 用户名是哈哈，年龄20！
+        user.setUname("哈哈");
+        user.setAge(20);
+        //---以上是用户前台提交的数据！
+        user.setDate( new Date() ); //填充populate、补全 这个缺失的date数据
+
+        //--关键：在这个公共map中，提供已知键名"abc"，存入这个完整的user
+        map.put( "abc", user );
+        //无返回值
+    }
+
+    @RequestMapping( "/testModelAttribute" )
+    //这里的user不用加@RequestParam注解，因为前台传参键名没有传user对象过来啊！
+    //注意：@ModelAttribute 的name属性等价于value属性，而只写value属性时，可以省略 value= ，直接写 "前台传递参数键名" 即可
+    public String testModelAttribute( @ModelAttribute(name="abc") User user ){ //等待 @ModelAttribute注解预处理后的数据，填进来user
+        System.out.println( user );
         return "success";
     }
 
