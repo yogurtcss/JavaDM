028b69fcf1b82feef81a91eab5ead779a1b7b9c0
yogurtcss <1806631955@qq.com>
2020-02-02 15:08:28 +0800

    upload

---
+++ Projects/MavenProjects/CommonProjects/SpringBoot_all/SpringBoot1/src/main/java/pers/yo/Controller/QuickController.java
@@ -0,0 +1,15 @@
+package pers.yo.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class QuickController {
+
+    @RequestMapping( "/quick" )
+    @ResponseBody
+    public String quick(){
+        return "Hello SpringBoo213t";
+    }
+}
---
+++ Projects/MavenProjects/CommonProjects/SpringBoot_all/SpringBoot1/src/main/java/pers/yo/MySpringBootApplication.java
@@ -0,0 +1,11 @@
+package pers.yo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication //SpringBoot的引导类
+public class MySpringBootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run( MySpringBootApplication.class );
+    }
+}
