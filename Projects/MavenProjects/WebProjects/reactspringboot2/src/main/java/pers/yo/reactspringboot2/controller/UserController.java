4c589ada26de37d59da6f2dbec7675b4983394e4
yogurtcss <1806631955@qq.com>
2020-02-04 23:06:47 +0800

    React_SpringBoot2

--- Projects/MavenProjects/WebProjects/reactspringboot2/src/main/java/pers/yo/reactspringboot2/controller/UserController.java
+++ Projects/MavenProjects/WebProjects/reactspringboot2/src/main/java/pers/yo/reactspringboot2/controller/UserController.java
@@ -2,6 +2,7 @@ package pers.yo.reactspringboot2.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import pers.yo.reactspringboot2.domain.User;
@@ -10,11 +11,17 @@ import pers.yo.reactspringboot2.service.UserService;
 import java.util.List;
 
 @Controller
+@CrossOrigin
 @RequestMapping( "/user" )
 public class UserController {
 
     @Autowired
     private UserService us;
+
+    @RequestMapping("/homePage")
+    public String homePage(){
+        return "index";
+    }
 
     @RequestMapping( "/findAll" )
     public @ResponseBody List<User> findAll(){
---
+++ Projects/MavenProjects/WebProjects/reactspringboot2/src/main/resources/static/precache-manifest.f65fa8b376f4ab5a5ea58f0dbac9e19b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "102c6fc04e03ca859f0d8a59ffe05e1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04758f2b93a4a101393a",
+    "url": "/static/js/2.060053b9.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/static/js/2.060053b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08156fd3df438c914c6e",
+    "url": "/static/js/main.a3c18962.chunk.js"
+  },
+  {
+    "revision": "0293b7e9c5922b599934",
+    "url": "/static/js/runtime-main.79b89f54.js"
+  }
+]);
