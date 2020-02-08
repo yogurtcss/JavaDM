18ea815a64585770d24afcbd1f6101be94a0e75d
yogurtcss <1806631955@qq.com>
2020-02-08 13:15:56 +0800

    bwcar1

--- Projects/MavenProjects/WebProjects/bwcar1/src/main/java/pers/yo/bwcar1/service/MenuService.java
+++ Projects/MavenProjects/WebProjects/bwcar1/src/main/java/pers/yo/bwcar1/service/MenuService.java
@@ -2,8 +2,24 @@ package pers.yo.bwcar1.service;
 
 import pers.yo.bwcar1.dto.DataGridResult;
 import pers.yo.bwcar1.dto.QueryDTO;
+import pers.yo.bwcar1.pojo.SysMenu;
+import pers.yo.bwcar1.utils.R;
+
+import java.util.List;
 
 public interface MenuService {
     //针对 bootstrap要求填入后端数据的格式，特地整出了这个DataGridResult类
     public abstract DataGridResult findMenu( QueryDTO queryDTO );
+
+    //新增方法
+    public abstract R deleteMenu(List<Long> ids );
+
+    //selectMenu()对应dao接口的方法：findMenu()
+    public abstract R selectMenu();
+
+    public abstract R saveMenu( SysMenu sysMenu );
+
+    public abstract R findMenuById( Long menuId );
+
+    public abstract R updateMenu( SysMenu sysMenu );
 }
