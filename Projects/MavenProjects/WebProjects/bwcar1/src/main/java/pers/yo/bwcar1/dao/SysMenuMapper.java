34af2af0ef7347e8f3809e80427d9e753d144f0d
yogurtcss <1806631955@qq.com>
2020-02-08 13:15:43 +0800

    bwcar1

--- Projects/MavenProjects/WebProjects/bwcar1/src/main/java/pers/yo/bwcar1/dao/SysMenuMapper.java
+++ Projects/MavenProjects/WebProjects/bwcar1/src/main/java/pers/yo/bwcar1/dao/SysMenuMapper.java
@@ -2,6 +2,7 @@ package pers.yo.bwcar1.dao;
 
 import java.util.List;
 import org.apache.ibatis.annotations.Param;
+import pers.yo.bwcar1.dto.QueryDTO;
 import pers.yo.bwcar1.pojo.SysMenu;
 import pers.yo.bwcar1.pojo.SysMenuExample;
 
@@ -27,4 +28,14 @@ public interface SysMenuMapper {
     int updateByPrimaryKeySelective(SysMenu record);
 
     int updateByPrimaryKey(SysMenu record);
+
+    /* 新增的方法findMenuByPage，已在 映射配置文件.xml中写好了
+    * 返回值也是 pojo类 SysMenu，传入的形参是查询参数QueryDTO --数据传输对象
+    *  */
+    public abstract List<SysMenu> findMenuByPage( QueryDTO queryDTO );
+
+    //新增的方法；List的泛型是包装类java.lang.Long 长整型
+    public abstract int deleteMenu( List<Long> ids );
+
+    public abstract List<SysMenu> findMenu();
 }
