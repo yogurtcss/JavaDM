ca46d8968baf1897b26bc3607deb4654c0ff445f
yogurtcss <1806631955@qq.com>
2020-02-28 22:54:30 +0800

    upload

---
+++ Projects/MavenProjects/WebProjects/yonews/src/test/java/pers/yo/yonews/test1.java
@@ -0,0 +1,11 @@
+package pers.yo.yonews;
+
+
+import org.junit.jupiter.api.Test;
+
+public class test1 {
+    @Test
+    public void test1(){
+        System.out.println( "xjb的news_id为：news/2020/02/28/696834" );
+    }
+}
